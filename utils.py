c3d36087f53ce5217c82c548e5011a26cf3f49fe
rojung <roland.jung@aau.at>
2020-08-27 12:05:03 +0200

    [TASK] package script_utils added

---
+++ utils.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def exit_success():
+    print("#########################   SUCCESS   #######################")
+    sys.exit(0)
+
+
+def exit_failure():
+    print("#########################   FAILURE   #######################")
+    sys.exit(1)
